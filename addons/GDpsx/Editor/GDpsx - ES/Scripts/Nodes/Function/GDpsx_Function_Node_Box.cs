b279dee9b9f66ee19c889c5705a368ef93752ba8
brandon <brandon@friendlystudios.net>
2024-02-29 10:50:51 -0600

    Implementing Reflective methods for event system

---
+++ addons/GDpsx/Editor/GDpsx - ES/Scripts/Nodes/Function/GDpsx_Function_Node_Box.cs
@@ -0,0 +1,36 @@
+using GDpsx_API.EventSystem;
+using Godot;
+using System;
+using System.Reflection;
+
+[Tool]
+public partial class GDpsx_Function_Node_Box : HBoxContainer
+{
+    [Export] public MenuButton functionMenu;
+    public override void _Ready()
+    {
+        Type type = typeof(GDpsx_ES_FunctionNodeLibrary);
+        MethodInfo[] methodInfos = type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+        GD.Print($"Number of methods: {methodInfos.Length}");
+        foreach (var method in methodInfos)
+        {
+            ParameterInfo[] parameterInfo = method.GetParameters();
+            int paramCount = parameterInfo.Length;
+            if (paramCount > 0)
+            {
+                string methodName = method.Name;
+                GD.Print($"Method: {methodName}, Number of Parameters: {paramCount}");
+                for (int i = 0; i < paramCount; i++)
+                {
+                    var paramType = parameterInfo[i].ParameterType;
+                    string paramName = parameterInfo[i].Name;
+                    GD.Print($"Parameter {i+1}: {paramName}, Parameter Type: {paramType}");
+                }
+            }
+            else
+            {
+                GD.Print($"Method: {method.Name} has no parameters");
+            }
+        }
+    }
+}
